121deaa8fd69fde49503b483c27ba19243451c02
MMadejsza <maciej.madejsza@gmail.com>
2023-02-17 11:14:59 +0000

    Table class only

--- js/main.js
+++ js/main.js
@@ -31,31 +31,4 @@ class TableRWD {
 	};
 }
 
-const values = [
-	{name: 'Ananas', calories: 33, fat: 0, carbs: 11.8},
-	{name: 'Jabłko', calories: 57, fat: 0.7, carbs: 12.1},
-	{name: 'Pomarańcza', calories: 51, fat: 0.2, carbs: 11.3},
-	{name: 'Wiśnie', calories: 67, fat: 0.4, carbs: 14.6},
-];
-
-const tableValuesContainer = document.querySelector('.app--values');
-
-const desktopViewport = window.matchMedia('screen and (min-width: 500px)');
-
-const drawTableValues = () => {
-	tableValuesContainer.innerHTML = '';
-	let table = document.createElement('table');
-	let thead = document.createElement('thead');
-	thead.innerHTML = `
-    <th>Name</th>
-    <th>Calories</th>
-    <th>Fat</th>
-    <th>Carbs1</th>
-    `;
-	table.appendChild(thead);
-
-	tableValuesContainer.appendChild(table);
-};
-
-drawTableValues();
 document.addEventListener('DOMContentLoaded', () => new TableRWD('.app--values'));
